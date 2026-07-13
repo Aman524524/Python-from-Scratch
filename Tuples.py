03c49551fee6b6cb0526eb1e0ddb480e0f6ec0d5
Abhinav Aman <194099996+Aman524524@users.noreply.github.com>
2026-07-14 00:33:50 +0530

    6.Tuples

--- Tuples.py
+++ Tuples.py
@@ -5,5 +5,17 @@
 colors = ("red", "green", "blue")
 colors2 = ("yellow", "orange")
 colors3 = colors + colors2 # concatenation of tuples #colors3 is also a tuple
+
 print(colors3) #('red', 'green', 'blue', 'yellow', 'orange')
+
 print(type(colors3)) #<class 'tuple'>
+
+slice=colors3[1:4] #slicing of tuples
+print(slice) #('green', 'blue', 'yellow')
+
+slice1=colors3[-3:-1] # -3 is the index of 'blue' and -1 is the index of 'orange' 
+#but it will not include the last index
+print(slice1) #('blue', 'yellow')
+
+slice2=colors3[0:5:2] # slicing with step #takes 1st index then 3rd index then 5th index and so on
+print(slice2) #('red', 'blue', 'orange')
