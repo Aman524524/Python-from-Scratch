ddf4568a5d4fbb82a9535329cbd8e2c8ecc55cc4
Abhinav Aman <194099996+Aman524524@users.noreply.github.com>
2026-07-14 00:17:59 +0530

    5.List

--- List.py
+++ List.py
@@ -1,14 +1,27 @@
 name=["aman","rahul","sachin","rohit"]
 mixed=[1,2.33,"aman",True]
+
 length=len(name)
 print(length)
+#4
 first=name[0] # Accessing by index
 print(first)
+#aman
 
 firstSecond=name[0:2] ## Accessing by slicings
 print(firstSecond)
+#['aman', 'rahul']
 
 print(type(mixed[3])) # here it iwill show the type of data present in that list
+#<class 'bool'>
 
 removeLast=name.pop() # it will remove the last element from the list
-print(name)
+print(name) #OPERATIONS ON LIST IS DONE LIKE IT IS UPDATED and go simultanesoustly
+#['aman', 'rahul', 'sachin'] 
+
+removeIndex=name.pop(1) # it will remove the element present at index 1
+print(name)#['aman', 'sachin']
+print(removeIndex) #rahul (shows the removed element/poped element)
+
+insertIndex=name.insert(1,"rohit") # it will insert the element at index 1
+print(name) #['aman', 'rohit', 'sachin'] # inserted the element at index 1 and updated the list
