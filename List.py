702e962cca7e89704e4e3a9e66320f468efe3b71
Abhinav Aman <194099996+Aman524524@users.noreply.github.com>
2026-07-14 00:07:47 +0530

    5.List

--- List.py
+++ List.py
@@ -2,8 +2,13 @@ name=["aman","rahul","sachin","rohit"]
 mixed=[1,2.33,"aman",True]
 length=len(name)
 print(length)
-first=name[0]
+first=name[0] # Accessing by index
 print(first)
-firstSecond=name[0:2]
+
+firstSecond=name[0:2] ## Accessing by slicings
 print(firstSecond)
+
 print(type(mixed[3])) # here it iwill show the type of data present in that list
+
+removeLast=name.pop() # it will remove the last element from the list
+print(name)
