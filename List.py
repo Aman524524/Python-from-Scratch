148a9ec318941d1adfe85717c4c11c7787cc2fbf
Abhinav Aman <194099996+Aman524524@users.noreply.github.com>
2026-07-14 00:03:46 +0530

    5.List

--- List.py
+++ List.py
@@ -1,7 +1,9 @@
 name=["aman","rahul","sachin","rohit"]
+mixed=[1,2.33,"aman",True]
 length=len(name)
 print(length)
 first=name[0]
-firstSecond=name[0:2]
 print(first)
+firstSecond=name[0:2]
 print(firstSecond)
+print(type(mixed[3])) # here it iwill show the type of data present in that list
