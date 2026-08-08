cd9669ba0073c97ccded890218bb2c08ab4fc913
Abhinav Aman <194099996+Aman524524@users.noreply.github.com>
2026-08-08 20:46:24 +0530

    Functions

--- functions.py
+++ functions.py
@@ -1,25 +1,109 @@
-#no arguments but return value
-def greet():
-    return "Hello, welcome to our program!"
+# """ #no arguments but return value
+# def greet():
+#     return "Hello, welcome to our program!"
 
-greet2 = greet()
-print(greet2)
+# greet2 = greet()
+# print(greet2)
 
-#no arguments no return value
-def greet():
-    print("hello")
+# #no arguments no return value
+# def greet():
+#     print("hello")
 
-greet()
+# greet()
 
-#arguments but no return value
-def greet(name):
-    print("Hello,"+name)
+# #arguments but no return value
+# def greet(name):
+#     print("Hello,"+name)
 
-greet("Automation Anywhere")
+# greet("Automation Anywhere")
 
-#arguemnts and return value
-def greet(name):
-    return "Hello,"+name
-greet1 = greet("Automation Anywhere")
+# #arguemnts and return value
+# def greet(name):
+#     return "Hello,"+name
+# greet1 = greet("Automation Anywhere")
 
-print(greet1)
+# print(greet1)
+
+
+
+# #####################################################3
+# def missingNumber(arr):
+#     n = len(arr)
+#     flag = False
+
+#     # Check if 1 is present in array or not
+#     for i in range(n):
+#         if arr[i] == 1:
+#             flag = True
+#             break
+
+#     # If 1 is not present
+#     if not flag:
+#         return 1
+
+#     # Change out of range values to 1
+#     for i in range(n):
+#         if arr[i] <= 0 or arr[i] > n:
+#             arr[i] = 1
+
+#     # Mark the occurrence of numbers 
+#     # directly within the same array
+#     for i in range(n):
+#         arr[(arr[i] - 1) % n] += n
+
+#     # Finding which index has value less than n
+#     for i in range(n):
+#         if arr[i] <= n:
+#             return i + 1
+
+#     # If array has values from 1 to n
+#     return n + 1
+
+# if __name__ == "__main__":
+#     arr = [2, -3, 4, 1, 1, 7]
+#     print(missingNumber(arr))
+
+#  """
+
+# city=["london","new york","Delhi","tokyo"]
+# Numbers=[1,2,3,4,43,21]
+# def LenOf(a):
+#     print(len(a))
+
+# LenOf(city)
+# LenOf(Numbers)
+# 4
+# 6
+
+# Numbers=[2,3,4,43,21]
+
+# def ListPrint(list):
+#  for i in list:
+#   print(i,end=" ")
+  
+# ListPrint(Numbers)
+
+# def factorial(n):
+#     for i in range(1,n):
+#         n=i*n
+#     print(n)
+
+# factorial(5)
+# 120
+
+# def converter(usd):
+#     inr=usd*98.47
+#     print(inr)
+
+# converter(2)
+# 196.94
+
+
+def OddEvenCheck():
+    n=int(input("Enter a number: "))
+    if n%2==0:
+     print(f"{n} is even")
+    else:
+     print(f"{n} is odd")
+
+OddEvenCheck()
